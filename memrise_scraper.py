c0cd26a9245e7d062b1f444e67426a40bb3c01d4
Thomas Branfield <tbranfield@gmail.com>
2023-08-08 18:38:31 +0100

    Added widgets for URL/filename/separator selection, and labels

--- memrise_scraper.py
+++ memrise_scraper.py
@@ -14,42 +14,48 @@ class MemriseScraper(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi(str(Path(__file__).parents[0] / "memrise_scraper.ui"), self)
-
         self.url = ""
+        self.separator = ","
+        self.out_filename = ""
         self.connect_widgets()
 
-        def connect_widgets():
-            self.url_entry.textChanged.connect(self.url_entry_changed)
-            self.clear_button.clicked.connect(self.clear_url)
-            self.output_chooser
+    def connect_widgets(self):
+        self.url_entry.textChanged.connect(self.url_entry_changed)
+        self.clear_button.clicked.connect(self.clear_url)
+#        self.browse_button
+#        self.output_filename_label
+#        self.separator_box
 
-        def url_entry_changed(self, url_entry_string):
-            self.url = url_entry_string
 
-        def clear_url(self):
-            """Clears the url"""
-            self.url = ""
-            self.url_entry.setText("")
+    def url_entry_changed(self, url_entry_string):
+        self.url = url_entry_string
+        print(self.url)
 
-        def scrape(self):
-            """Scrapes the words from the given url"""
+    def clear_url(self):
+        """Clears the url"""
+        print("clearing")
+        self.url = ""
+        self.url_entry.setText("")
 
-            url = "https://app.memrise.com/course/2158097/chemistry-of-rocks-and-minerals/"
-            page = urlopen(url)
-            html = page.read().decode("utf-8")
-            soup = BeautifulSoup(html, "html.parser")
+    def scrape(self):
+        """Scrapes the words from the given url"""
 
-            results = soup.find_all(lambda tag: tag.name == "div" and
+        url = "https://app.memrise.com/course/2158097/chemistry-of-rocks-and-minerals/"
+        page = urlopen(url)
+        html = page.read().decode("utf-8")
+        soup = BeautifulSoup(html, "html.parser")
+
+        results = soup.find_all(lambda tag: tag.name == "div" and
                                        tag.get("class") == ["text"])
-            word_pairs = []
+        word_pairs = []
 
-            it = iter(results)
-            for element in it:
-                tested_word = element.text
-                english_word = next(it).text
-                word_pairs.append((tested_word, english_word))
+        it = iter(results)
+        for element in it:
+            tested_word = element.text
+            english_word = next(it).text
+            word_pairs.append((tested_word, english_word))
 
-                print(word_pairs)
+            print(word_pairs)
 
 
 

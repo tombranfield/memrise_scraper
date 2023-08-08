c40d1e0f5911630e9d50e6da772304c6cad18b2b
Thomas Branfield <tbranfield@gmail.com>
2023-08-08 16:14:44 +0100

    Removed word_pair

--- memrise_scraper.py
+++ memrise_scraper.py
@@ -4,6 +4,7 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
+from word_pair import WordPair
 
 # Want to choose URL
 # Want to choose seperator
@@ -17,6 +18,15 @@ soup = BeautifulSoup(html, "html.parser")
 results = soup.find_all(lambda tag: tag.name == "div" and
                                     tag.get("class") == ["text"])
 
+word_pairs = []
+
 it = iter(results)
 for element in it:
     print(element.text, next(it).text)
+    tested_word = element.text
+    english_word = next(it).text
+    word_pair = WordPair(tested_word, english_word)
+    word_pairs.append((tested_word, english_word))
+
+
+print(word_pairs)

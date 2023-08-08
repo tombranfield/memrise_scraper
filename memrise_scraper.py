4eb936a002037cd6067571b0f51b2d12aa239092
Thomas Branfield <tbranfield@gmail.com>
2023-08-08 17:42:30 +0100

    memrise_scrapper is now in a QMainWindow

--- memrise_scraper.py
+++ memrise_scraper.py
@@ -2,28 +2,42 @@
 
 
 from bs4 import BeautifulSoup
+from pathlib import Path
 from urllib.request import urlopen
+import sys
 
-# Want to choose URL
-
-# Want to choose seperator (for output file)
-# Choose output file name and location
-
-url = "https://app.memrise.com/course/2158097/chemistry-of-rocks-and-minerals/"
-page = urlopen(url)
-html = page.read().decode("utf-8")
-soup = BeautifulSoup(html, "html.parser")
-
-results = soup.find_all(lambda tag: tag.name == "div" and
-                                    tag.get("class") == ["text"])
-
-word_pairs = []
+from PyQt5 import QtWidgets, uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
 
 
-it = iter(results)
-for element in it:
-    tested_word = element.text
-    english_word = next(it).text
-    word_pairs.append((tested_word, english_word))
+class MemriseScraper(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi(str(Path(__file__).parents[0] / "memrise_scraper.ui"), self)
 
-print(word_pairs)
+
+        url = "https://app.memrise.com/course/2158097/chemistry-of-rocks-and-minerals/"
+        page = urlopen(url)
+        html = page.read().decode("utf-8")
+        soup = BeautifulSoup(html, "html.parser")
+
+        results = soup.find_all(lambda tag: tag.name == "div" and
+                                       tag.get("class") == ["text"])
+
+        word_pairs = []
+
+
+        it = iter(results)
+        for element in it:
+            tested_word = element.text
+            english_word = next(it).text
+            word_pairs.append((tested_word, english_word))
+
+            print(word_pairs)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MemriseScraper()
+    window.show()
+    app.exec_()

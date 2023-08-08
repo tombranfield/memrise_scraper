611c15d822972fbdad6a501f048acbc2931d408a
Thomas Branfield <tbranfield@gmail.com>
2023-08-08 15:50:23 +0100

    Used a custom function to sort by exact class attribute

--- memrise_scraper.py
+++ memrise_scraper.py
@@ -4,8 +4,22 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
-url = https://app.memrise.com/course/2158097/chemistry-of-rocks-and-minerals/
+# Will provide a means to enter a URL later... for now, just testing
+url = "https://app.memrise.com/course/2158097/chemistry-of-rocks-and-minerals/"
 page = urlopen(url)
 html = page.read().decode("utf-8")
 soup = BeautifulSoup(html, "html.parser")
 
+print(soup.title.string)
+
+
+results = soup.find_all(lambda tag: tag.name == "div" and
+                                    tag.get("class") == ["text"])
+
+
+
+print(results)
+
+
+
+#print(my_divs)

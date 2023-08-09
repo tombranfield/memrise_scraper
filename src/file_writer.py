9e9f8497f6df02da867c5c68e63417a2f4a322a2
Thomas Branfield <tbranfield@gmail.com>
2023-08-09 17:34:46 +0100

    Added file_writer.py

---
+++ src/file_writer.py
@@ -0,0 +1,15 @@
+"""file_writer.py"""
+
+
+class FileWriter:
+    """Responsible for saving the wordpairs to a file"""
+    def __init__(self, word_pairs, separator, output_filename):
+        self.word_pairs = word_pairs
+        self.separator = separator
+        self.output_filename = output_filename
+        
+    def write_to_file(self):
+        with open(self.output_filename, "w") as out_file:
+            for pair in self.word_pairs:
+                line = pair[0] + self.separator + pair[1] + "\n"
+                out_file.write(line)
